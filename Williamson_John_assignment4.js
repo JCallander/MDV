8e7ce7903313a323454af97ef4113e5e2043fcd7
John Williamson <jcallander@fullsail.edu>
2013-06-24 05:15:24 -0500

    rev 2
    Added 2 more functions to see if an email adress is correct and to see
    if a web address is begins with http: or https:

--- Williamson_John_assignment4.js
+++ Williamson_John_assignment4.js
@@ -1,13 +1,13 @@
 //alert("JavaScript works!");
 
 //This set turns each word in a string into an array based off of the spaces.
-
+/*
 var myString = "Man this is some hard work.";
 var myTextArray = myString.split(' ');
 console.log(myTextArray);
 
 //This set will not let you enter anything but a phone number
-/*
+
 var myString = prompt("Please enter Your Phone number", "xxx-xxx-xxxx");
 
 function checkCharType(charToCheck){
@@ -38,10 +38,9 @@ switch (checkCharType(myString))
 	default:
 		alert("Thank You");
 }
-*/
-
 
 //This set will tell you if the value of something is of the correct pattern. 
+
 var isANumber = true
 function phoneNumber(phone){
 var areaCode = phone.substring(0,3);
@@ -51,10 +50,41 @@ var dash2 = phone.substring(7,8);
 var lastFour = phone.substring(8,12);
 
  if (isNaN(areaCode) || isNaN(cityCode) || isNaN(lastFour) || dash1 != "-" || dash2 != "-") {
-        isANumber = false
-		return isANumber
+        isANumber = false;
+		return isANumber;
   }
 }
-phoneNumber("313-111-3321")
+phoneNumber("313-111-3341");
 console.log(isANumber)
 
+//This set will verify that an email address is in the correct format.
+
+var anEmail = true;
+function validateEmail(email){
+var e = email;
+var atSign = e.indexOf("@");
+var dotCom = e.lastIndexOf(".");
+if (atSign < 1 || dotCom < atSign + 2 || dotCom + 2 >= e.length){
+  anEmail = false;
+  return anEmail;
+  }
+}
+validateEmail("john@doe.com");
+console.log(anEmail);
+
+
+//This set will check to see if a web address begins with "http:" or "https:"
+
+var anURL = false;
+function validWebSite(url){
+var u = url;
+var prefix = u.substring(0,(u.indexOf("/")));
+if (prefix.match("http:") || prefix.match("https:")){
+  anURL = true;
+  return anURL;
+  }
+}
+validWebSite("https://www.johan.com");
+console.log(anURL);
+
+*/
